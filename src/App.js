c849b8b91d202050fb1ee4be536549f71eac8d8b
Wali Muhammad <wm401238@gmail.com>
2023-09-01 16:16:38 +0500

    M-ride Website is start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from '@mui/material/Button';
+import ResponsiveAppBar from './Navbar Component/ResponsiveAppBar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    return(
+        <div>
+           <Button variant="contained">Hello world</Button>
+           <ResponsiveAppBar/>
+        </div>
+    )
 }
 
-export default App;
+export default App
